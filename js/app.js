aebaae51d7e0b89bac56a5ef475f1db993e1ab21
Dingwen <1835246993@qq.com>
2019-10-05 11:37:15 +0800

    Site updated: 2019-10-05 11:37:15

--- js/app.js
+++ js/app.js
@@ -1,33 +1,3 @@
-const getRealPath = (pathname, desc = false) => {
-  if (!pathname) {
-    pathname = window.location.pathname;
-  }
-  let names = pathname.split("/");
-  if (desc === false) {
-    for (let i = names.length - 1; i >= 0; --i) {
-      let name = names[i].trim();
-      if (name.length > 0 && name !== "/" && name !== "index.html") {
-        return name;
-      }
-    }
-  } else {
-    for (let i = 0; i < names.length; ++i) {
-      let name = names[i].trim();
-      if (name.length > 0 && name !== "/" && name !== "index.html") {
-        return name;
-      }
-    }
-  }
-  return "/";
-};
-let links = document.querySelectorAll(".nexmoe-list-item");
-let rootRealPath = getRealPath(window.location.pathname, true);
-for (let link of links) {
-  let linkPath = link.getAttribute("href");
-  if (linkPath && getRealPath(linkPath, true) === rootRealPath) {
-    link.className = "active nexmoe-list-item mdui-list-item mdui-ripple";
-  }
-}
 $("table")
   .has("img")
   .addClass("nexmoe-album");
@@ -43,8 +13,54 @@ $("article:not(.nexmoe-py) img").each(function() {
   var element = document.createElement("a");
   $(element).attr("data-fancybox", "gallery");
   $(element).attr("href", $(this).attr("data-src"));
+  $(element).attr("title", $(this).attr("alt"));
   $(this).wrap(element);
 });
 
 $("#nexmoe-sidebar a").addClass("mdui-ripple");
 mdui.mutation();
+
+/*
+ 随机封面图
+ */
+
+let covers = $(".post-rand-cover");
+
+for (let i = 0; i < covers.length; i++){
+  covers[i].src = "https://uuz-blog.oss-cn-hangzhou.aliyuncs.com/images/post-covers/" + Math.round(Math.random() * (7-1)) + ".jpg";
+}
+
+/*
+侧边栏跟随
+ */
+
+let drawer = $(".nexmoe-drawer")[0];
+
+let scroll_hight_provous = 0;
+
+document.onscroll = function (){
+  let scroll_hight_now = document.body.scrollTop + document.documentElement.scrollTop;
+
+  //console.log("pvs: " + scroll_hight_provous + " ||| now: " + scroll_hight_now)
+
+  // 自动往下
+  if (scroll_hight_now > scroll_hight_provous && scroll_hight_now > $(".nexmoe-primary")[0].scrollHeight*0.2){
+    drawer.style.position = "fixed";
+    $(drawer).css("top", "auto");
+
+    scroll_hight_provous = scroll_hight_now;
+  }
+  
+  if (scroll_hight_now < scroll_hight_provous) {
+    drawer.style.position = "absolute";
+    $(drawer).css("top", scroll_hight_now);
+    //$(drawer).css("top", document.body.scrollTop);
+    //$(drawer).css("top", document.body.scrollTop + document.documentElement.scrollTop);
+
+    scroll_hight_provous = scroll_hight_now;
+  }
+
+
+
+};
+
